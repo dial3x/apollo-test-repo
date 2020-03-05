aa07a8e49504395578546b13034e8b424a478f87
Alex Chan <achan@shine.com.au>
2020-03-05 14:32:47 +1000

    added port node env

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ const resolvers = require('./resolvers/index');
 const server = new ApolloServer({ typeDefs, resolvers });
 
 // The `listen` method launches a web server.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
 });
